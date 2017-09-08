181d27eb2eeefb0056ac661df88b2a14b8dbdbfd
cailingyun <ingyun.cai@sth-telecom.com>
2017-09-08 08:42:03 +0800

    new ui for my data page

--- app/src/main/java/com/zpower/ui/fragments/MyDataFragment.java
+++ app/src/main/java/com/zpower/ui/fragments/MyDataFragment.java
@@ -6,9 +6,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.zpower.R;
+
+import static com.zpower.R.id.iv_back;
 
 /**
  * Created by zx on 2017/3/6.
@@ -17,7 +20,8 @@ import com.zpower.R;
 public class MyDataFragment extends BaseFragment implements View.OnClickListener {
 
     private View rootView;
-    private ImageView iv_switch_bg;
+    private TextView tv_best,tv_history;
+    private LinearLayout divider_line_best,divider_line_history;
     private DataHistoryFragment dataHistoryFragment;
     private DataBestFragment dataBestFragment;
 
@@ -33,13 +37,16 @@ public class MyDataFragment extends BaseFragment implements View.OnClickListener
     }
 
     private void initView() {
-        TextView tv_best = (TextView) rootView.findViewById(R.id.tv_best);
-        TextView tv_history = (TextView) rootView.findViewById(R.id.tv_history);
+        tv_best = (TextView) rootView.findViewById(R.id.tv_best);
+        tv_history = (TextView) rootView.findViewById(R.id.tv_history);
+        divider_line_best=(LinearLayout)rootView.findViewById(R.id.divider_line_best);
+        divider_line_history=(LinearLayout)rootView.findViewById(R.id.divider_line_history);
         ImageView iv_back = (ImageView) rootView.findViewById(R.id.iv_back);
-        iv_switch_bg = (ImageView) rootView.findViewById(R.id.iv_switch_bg);
         iv_back.setOnClickListener(this);
         tv_best.setOnClickListener(this);
         tv_history.setOnClickListener(this);
+        divider_line_best.setOnClickListener(this);
+        divider_line_history.setOnClickListener(this);
         dataBestFragment = new DataBestFragment();
         dataHistoryFragment = new DataHistoryFragment();
         replaceLoadRootFragment(R.id.data_frameLayout, dataBestFragment,true);
@@ -48,15 +55,21 @@ public class MyDataFragment extends BaseFragment implements View.OnClickListener
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.iv_back:
+            case iv_back:
                 pop();
                 break;
             case R.id.tv_best:
-                iv_switch_bg.setImageDrawable(getResources().getDrawable(R.mipmap.best));
+                tv_best.setTextColor(getResources().getColor(R.color.btn_bg));
+                tv_history.setTextColor(getResources().getColor(R.color.white));
+                divider_line_best.setVisibility(View.VISIBLE);
+                divider_line_history.setVisibility(View.INVISIBLE);
                 replaceLoadRootFragment(R.id.data_frameLayout,dataBestFragment,true);
                 break;
             case R.id.tv_history:
-                iv_switch_bg.setImageDrawable(getResources().getDrawable(R.mipmap.history));
+                tv_best.setTextColor(getResources().getColor(R.color.white));
+                tv_history.setTextColor(getResources().getColor(R.color.btn_bg));
+                divider_line_best.setVisibility(View.INVISIBLE);
+                divider_line_history.setVisibility(View.VISIBLE);
                 replaceLoadRootFragment(R.id.data_frameLayout,dataHistoryFragment,true);
                 break;
         }
