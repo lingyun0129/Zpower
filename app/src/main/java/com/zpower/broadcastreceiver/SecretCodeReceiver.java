8ffff43125c4a04a22baf7d3c55a1c1d05468759
cailingyun <ingyun.cai@sth-telecom.com>
2017-12-11 14:11:50 +0800

    加入磁控校准页面

--- app/src/main/java/com/zpower/broadcastreceiver/SecretCodeReceiver.java
+++ app/src/main/java/com/zpower/broadcastreceiver/SecretCodeReceiver.java
@@ -4,7 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-import com.zpower.ui.UnitCalibrationActivity;
+import com.zpower.ui.MagneticCalibrationActivity;
 
 public class SecretCodeReceiver extends BroadcastReceiver {
     private static String SECRET_CODE_ACTION = "android.provider.Telephony.SECRET_CODE";
@@ -13,7 +13,8 @@ public class SecretCodeReceiver extends BroadcastReceiver {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
         if(intent.getAction().equals(SECRET_CODE_ACTION)){
-                Intent secretIntent=new Intent(context, UnitCalibrationActivity.class);
+                //Intent secretIntent=new Intent(context, UnitCalibrationActivity.class);
+                Intent secretIntent=new Intent(context, MagneticCalibrationActivity.class);
                 secretIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startActivity(secretIntent);
         }
